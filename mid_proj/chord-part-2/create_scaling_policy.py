7b3c2dd061ed9f4f3955ff3dbbecd780d86c4702
Sam Lee <57612267+samli50801@users.noreply.github.com>
2023-04-27 00:11:21 +0800

    update

--- mid_proj/chord-part-2/create_scaling_policy.py
+++ mid_proj/chord-part-2/create_scaling_policy.py
@@ -1,7 +1,10 @@
 import boto3
+import requests
+
+inst_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
 
 # Create CloudWatch client
-cloudwatch = boto3.client('cloudwatch')
+cloudwatch = boto3.client('cloudwatch', 'us-east-1')
 
 # Create alarm
 cloudwatch.put_metric_alarm(
@@ -10,16 +13,18 @@ cloudwatch.put_metric_alarm(
     EvaluationPeriods=1,
     MetricName='CPUUtilization',
     Namespace='AWS/EC2',
-    Period=5,
+    Period=300,
     Statistic='Average',
     Threshold=70.0,
-    ActionsEnabled=False,
+    ActionsEnabled=True,
+    AlarmActions=[
+      'arn:aws:autoscaling:us-east-1:666687831115:scalingPolicy:b1da9d3f-2ddf-4117-abf6-b7370eeb98f5:autoScalingGroupName/chord_ASG:policyName/policy-1'
+    ],
     AlarmDescription='Alarm when server CPU exceeds 70%',
     Dimensions=[
         {
-          'Name': 'InstanceId',
-          'Value': 'INSTANCE_ID'
+          'Name': 'AutoScalingGroupName',
+          'Value': 'chord_ASG'
         },
-    ],
-    Unit='Seconds'
+    ]
 )
